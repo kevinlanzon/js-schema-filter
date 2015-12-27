2a183fcffb1cf137e5fd5a7f73b8f695c80f56bc
Kevin Lanzon <kevinlanzon@gmail.com>
2015-12-27 16:30:39 +0000

    updates readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 JSON Schema Filter
 ========
+
+🚧 IN PROGRESS 🚧
+
+A very basic JavaScript filter that takes JSON API in and outputs JSON API, with a filter by type and by field in the middle.
+
+###Setup
+
+```sh
+$ git clone git@github.com:kevinlanzon/js-schema-filter.git
+$ cd js-schema-filter
+$ npm install
+```
+
+###How to run the tests
+
+```sh
+$ npm start
+```
+
+###Technologies used
+
+- Node.js
+- jasmine-node
+- JSON
--- spec/filterSpec.js
+++ spec/filterSpec.js
@@ -2,42 +2,42 @@ var filter = require('../js/filter');
 
 describe('JSON Filter', function() {
 
-    var schema = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "title": "User",
-  "description": "User data for language learning application",
-  "type": "object",
-  "properties": {
-    "first_name":     {"type": "string"},
-    "last_name":      {"type": "string"},
-    "age": {
-      "description": "Given in years",
-      "type": "number",
-      "minimum": 0
-    },
-    "contact_number": {"type": "string"},
-    "email":          {"type": "string"},
-    "address": {
-      "type": "object",
-      "properties": {
-        "street_address": {"type": "string"},
-        "city":           {"type": "string"},
-        "state":          {"type": "string"},
-        "country":        {"type": "string"}
+  var schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "User",
+    "description": "User data for language learning application",
+    "type": "object",
+    "properties": {
+      "first_name":     {"type": "string"},
+      "last_name":      {"type": "string"},
+      "age": {
+        "description": "Given in years",
+        "type": "number",
+        "minimum": 0
+      },
+      "contact_number": {"type": "string"},
+      "email":          {"type": "string"},
+      "address": {
+        "type": "object",
+        "properties": {
+          "street_address": {"type": "string"},
+          "city":           {"type": "string"},
+          "state":          {"type": "string"},
+          "country":        {"type": "string"}
+        }
+      },
+      "languages": {
+        "description": "Languages spoken fluently",
+        "type": "array",
+        "items": {"type": "string"}
+      },
+      "newsletter": {
+        "description": "Has the user signed up for the newsletter?",
+        "type": "boolean"
       }
     },
-    "languages": {
-      "description": "Languages spoken fluently",
-      "type": "array",
-      "items": {"type": "string"}
-    },
-    "newsletter": {
-      "description": "Has the user signed up for the newsletter?",
-      "type": "boolean"
-    }
-  },
-  "required": ["first_name", "last_name", "email"]
-}
+    "required": ["first_name", "last_name", "email"]
+  }
 
   it('returns the complete json data', function() {
     var json = { "first_name" : "Kev" }
@@ -66,5 +66,4 @@ describe('JSON Filter', function() {
     expect(newJson).toEqual({ "first_name" : "Kevin",
       "last_name" : "Lanzon", "contact_number": "123456789"});
   });
-
 });
